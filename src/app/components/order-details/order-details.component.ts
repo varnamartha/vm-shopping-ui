2bafb398ade8ea625bc55d5a76cfae606201ad20
varnamartha <varnamartha@gmail.com>
2021-06-14 19:08:30 -0300

    Add order details component

--- src/app/components/order-details/order-details.component.ts
+++ src/app/components/order-details/order-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderServices } from 'src/app/services/order.services';
+import { ActivatedRoute} from '@angular/router'
+import { OrderResponse } from '../../models/orden-models';
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.sass']
 })
 export class OrderDetailsComponent implements OnInit {
+  
+  id= 0;
+  order?:OrderResponse;
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute, private orderServices: OrderServices) {  
+   
+    this.activatedRoute.params.subscribe(params =>
+      {
+        this.id = Number.parseInt(params['id']) ;
+      })
+      this.orderServices._getOrderDetails(this.id).subscribe(data =>{
+        this.order = data;
+      });
+   }
 
-  ngOnInit(): void {
+  ngOnInit( ): void {
   }
 
 }
