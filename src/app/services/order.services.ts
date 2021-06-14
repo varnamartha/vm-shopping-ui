0af8d198f402d0cd50473d92aa67759578177f5f
varnamartha <varnamartha@gmail.com>
2021-06-14 19:48:59 -0300

    Point order url services to environment variables

--- src/app/services/order.services.ts
+++ src/app/services/order.services.ts
@@ -1,32 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import { Order, OrderResponse } from '../models/orden-models';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class OrderServices
 {    
     orders : Array<Order> = [];
-    urlOrerservicesBase = "https://localhost:44376/api/Order";
+    orderUrlBase = `${environment.appUrl}/order`;
 
     constructor (private _httpClient: HttpClient)
     {       
-        console.log("OrderServices listo!!")
     }
 
     _postOrder(order: Order)
     {
-        return this._httpClient.post<OrderResponse>(this.urlOrerservicesBase,order);
+        return this._httpClient.post<OrderResponse>(this.orderUrlBase,order);
     }
 
     _getOrdersByEmail(email:string)
     {
-        let url = this.urlOrerservicesBase + '/list?email=' + email;
+        let url = `${this.orderUrlBase}/list?email=${email}`;
         return this._httpClient.get<OrderResponse[]>(url);
     }
 
     _getOrderDetails(orderId:number)
     {
-        let url = this.urlOrerservicesBase + '/' + orderId;
+        let url = `${this.orderUrlBase}/${orderId}`;
         return this._httpClient.get<OrderResponse>(url);
     }
 }
