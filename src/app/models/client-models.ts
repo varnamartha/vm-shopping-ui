1b46f6b9967f42050df29785603d1bc750fd2c9b
varnamartha <varnamartha@gmail.com>
2021-06-14 11:23:22 -0300

    Add client model

---
+++ src/app/models/client-models.ts
@@ -0,0 +1,13 @@
+export class Client
+{
+    Name:string;
+    Mail :string;
+    Phone :string;
+     
+     constructor(Name:string, Mail :string, Phone :string)
+     {
+         this.Name= Name
+         this.Mail= Mail
+         this.Phone= Phone
+     }
+}
